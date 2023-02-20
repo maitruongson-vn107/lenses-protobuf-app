05874e106088a601f2e27dffdff465e839caa0d4
Truong Son Mai <sonmt.work00@gmail.com>
2023-02-21 01:08:35 +0700

    first commit

---
+++ src/main/java/kafka/SendKafkaProto.java
@@ -0,0 +1,56 @@
+package kafka;
+
+import com.github.javafaker.Faker;
+import java.util.Random;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import com.example.CardData;
+import java.util.Properties;
+
+public class SendKafkaProto {
+    public static void main(String[] args) {
+        // Setup Producer Properties
+        String bootstrapServers = "localhost:29092";
+        var properties = new Properties();
+        properties.setProperty("bootstrap.servers", bootstrapServers);
+        properties.setProperty("schema.registry.url", "http://localhost:8085");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", KafkaProtobufSerializer.class.getName());
+
+        KafkaProducer<String, CardData.CreditCard> producer = new KafkaProducer<>(properties);
+        // Specify Topic Name
+        var topic = "protos_topic_cards";
+
+        // Loop to Produce Fake Data
+        for (int i = 0; i < 15; i++) {
+            // creating Random object
+            Random rd = new Random();
+            Faker faker = new Faker();
+            String name = faker.name().fullName();
+            String countryCode = faker.address().countryCode();
+            String cardNumber = faker.business().creditCardNumber();
+            Integer typeValue = rd.nextInt(3);
+            String currencyCode = faker.country().currencyCode();
+
+            // Serializing to Protobuf based on CreditCard.proto Schema
+            var cardData = CardData.CreditCard.newBuilder()
+                    .setName(name)
+                    .setCountry(countryCode)
+                    .setCurrency(currencyCode)
+                    .setTypeValue(typeValue)
+                    .setBlocked(false)
+                    .setCardNumber(cardNumber)
+                    .build();
+
+            var record = new ProducerRecord<String, CardData.CreditCard>(topic, "Credit Card", cardData);
+            // Send to Producer
+            producer.send(record);
+        }
+        producer.flush();
+        producer.close();
+        // Log success message
+        System.out.println("Sent Data Successfully");
+    }
+}
